0a10ecc337a5fad7278dc710ed61d106a462590a
pavan-git <pavanwaghmare81@gmail.com>
2024-05-09 14:26:39 +0530

    conversion

---
+++ 03_conversion.js
@@ -0,0 +1,24 @@
+// const data = 662
+// console.log(typeof data)
+
+// let score = undefined
+// console.log(typeof score)
+
+
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber)
+
+// console.log(valueNumber)
+
+
+let store = 1;
+let isStore = Boolean(store)
+console.log(isStore)
+
+let unde = undefined;
+let isUnde = Number(unde)
+console.log(isUnde)
+
+let someNumber =64
+let findNumber = String(someNumber)
+console.log(typeof findNumber)
