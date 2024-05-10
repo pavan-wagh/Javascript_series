80ebc96ac1aaacf74439e3997e26d4e0a2625516
pavan-git <pavanwaghmare81@gmail.com>
2024-05-10 16:20:55 +0530

    math

---
+++ 06_numbers_maths.js
@@ -0,0 +1,19 @@
+const num = 500
+console.log(num.toFixed(2)) // 500.00
+
+const score = 124.98
+console.log(score.toPrecision(3)) // 125
+
+
+// *** Maths ***
+  
+// console.log(Math)
+// console.log(Math.abs(-4)) // 4
+// console.log(Math.round(4.6))  // 5
+// console.log(Math.floor(4.6))  // 4
+// console.log(Math.min(4,9,3,6))  
+// console.log(Math.max(4,8,1,6))  
+
+console.log(Math.random())
+console.log((Math.random()*10) +1)
+
